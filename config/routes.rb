e34c0d934255790bc49bd357f04906f39dc38e39
Benny Paulino <santiagopro71@gmail.com>
2018-03-10 15:50:58 -0500

    Adjust config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :users do
   end
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth' }
   root "tweets#index"
 end

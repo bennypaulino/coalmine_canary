73152973c4fdff3e00f87acab2d505a77803761d
Benny Paulino <santiagopro71@gmail.com>
2018-03-11 15:50:10 -0400

    Show history, rename social_news.rb to social_tool.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,10 @@
 class PagesController < ApplicationController
   def home
+    @history = SocialTool.user_timeline(200)
   end
 
   def crypto_feed
-    @crypto_tweets = SocialNews.crypto_news
+    @crypto_tweets = SocialTool.crypto_news
     @user = current_user
   end
 end
--- lib/social_tool.rb
+++ lib/social_tool.rb
@@ -1,14 +1,18 @@
-module SocialNews
-  def self.crypto_news
-    client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV.fetch('TWITTER_APP_ID')
-      config.consumer_secret     = ENV.fetch('TWITTER_APP_SECRET')
-      config.access_token        = ENV.fetch('TWITTER_ACCESS_TOKEN')
-      config.access_token_secret = ENV.fetch('TWITTER_ACCESS_SECRET')
-    end
+module SocialTool
+  @@client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV.fetch('TWITTER_APP_ID')
+    config.consumer_secret     = ENV.fetch('TWITTER_APP_SECRET')
+    config.access_token        = ENV.fetch('TWITTER_ACCESS_TOKEN')
+    config.access_token_secret = ENV.fetch('TWITTER_ACCESS_SECRET')
+  end
 
-    client.search('#cryptocurrency', result_type: 'recent').take(6).collect do | tweet |
+  def self.crypto_news
+    @@client.search('#cryptocurrency', result_type: 'recent').take(6).collect do | tweet |
       "#{tweet.user.screen_name}: #{tweet.text}"
     end
+  end
+
+  def self.user_timeline(qty)
+    @@client.user_timeline(count: qty)
   end
 end

273d2bb52db0d778a863552b68236a2c6023bbfd
Benny Paulino <santiagopro71@gmail.com>
2018-03-12 22:02:44 -0400

    Adjust model and controller for Tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,26 +6,35 @@ class TweetsController < ApplicationController
 
   def new
     @tweet = Tweet.new
+    # @tweet.post_to_twitter(tweet_params[:body])
+    # if tweet_params[:tweet][:body].present?
+    if tweet_params[:body].present?
+      @tweet.post_to_twitter(tweet_params[:body])
+      redirect_to root_path, notice: 'Your soon to be viral tweet has been posted! Noice!!'
+    end
   end
 
   def create
-  @tweet = Tweet.create(tweet_params, user_id: current_user.id)
-  # @tweet.user_id = current_user.id
-  if @tweet.save
-    redirect_to root_path, notice: "Your tweet will definitely go viral! Noice!!"
-  else
-    render 'new'
-  end
-  respond_with(@tweet)
+    @tweet = Tweet.create(tweet_params, user_id: current_user.id)
+    # @tweet.user_id = current_user.id
+    if @tweet.save
+      redirect_to root_path, notice: 'Your tweet will definitely go viral! Noice!!'
+    else
+      render 'new'
+    end
+    respond_with(@tweet)
   end
 
   def update
-    @tweet.post_to_twitter(tweet_params)
+    if tweet_params[:body].present?
+      @tweet.post_to_twitter(tweet_params[:body])
+    end
     respond_with(@tweet)
   end
 
   private
+
   def tweet_params
-    params.require(:tweet).permit(:body)#, :user_id)
+    params.require(:tweet).permit(:body)
   end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,8 +1,12 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   #validates :user_id, presence: true
-  validates :body, presence: true, length: { maximum: 280 }
+  # validates :body, presence: true, length: { maximum: 280 }
   before_create :post_to_twitter
+
+  # def initialize(params={body: 'nil'})
+  #   @tweet = Tweet.new(params)
+  # end
 
   def post_to_twitter(msg)
     SocialTool.update(msg)
